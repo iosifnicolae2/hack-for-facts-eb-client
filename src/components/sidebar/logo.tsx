596c1a21c6276a84d1b95a4484224532e3f9e989
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2025-09-01 20:42:46 +0300

    feat: add logo

--- src/components/sidebar/logo.tsx
+++ src/components/sidebar/logo.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
-import { SquareSquare } from "lucide-react";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "../ui/sidebar";
 import { Link } from "@tanstack/react-router";
+import logo from "@/assets/logo/logo.png";
 
 const Logo: FC = () => {
   const { setOpenMobile } = useSidebar();
@@ -16,7 +16,7 @@ const Logo: FC = () => {
             asChild
           >
             <Link to="/" className="flex w-full items-center gap-2">
-              <SquareSquare className="h-6 w-6 text-black dark:text-white" />
+              <img src={logo} alt="Transparenta.eu" className="h-4 w-4 text-black dark:text-white" />
               <span
                 className="font-semibold text-lg whitespace-nowrap transition-opacity duration-200 group-data-[collapsible=icon]:opacity-0"
               >
